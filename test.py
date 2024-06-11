f65105cd71d908b55e4d0b6e110ac5d9f1a10fd5
jsh0246 <jsh0246@naver.com>
2024-06-11 16:03:44 +0900

    first

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print("pypy33")
+from bisect import bisect_left
+
+array = [10, 20, 15, 30, 20, 50]
+dp = [1]
+x = [array[0]]
+
+for i in range(1, len(array)):
+    if array[i] > x[-1]: # 현재 값이 x 배열의 마지막 값보다 클 경우
+        x.append(array[i]) # x 배열에 현재 값을 추가해 주고
+        dp.append(dp[-1] + 1) # 증가 부분 수열의 길이를 1 증가시킨다.
+    else: # 그렇지 않을 경우
+        idx = bisect_left(x, array[i]) # 현재 값이 x 배열의 몇 번째 인덱스에 들어갈 수 있는지를 찾아서
+        x[idx] = array[i] # x 배열의 idx 위치에 현재 값을 넣어준다.
+
+print(len(x))
+print(x)
